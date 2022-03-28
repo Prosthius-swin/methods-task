f6a39c23c10ef9da9dade95572e2320607342365
CALLUM HOPKINS <103919468@student.swin.edu.au>
2022-03-29 09:50:14 +1100

    completed one

---
+++ task/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sayHello();
+
+            static string sayHello() {
+                string hello = "Welcome Friends! \nHave a nice day!";
+                Console.WriteLine(hello);
+                return hello;
+            }
+            
+                
+        }
+    }
+}
